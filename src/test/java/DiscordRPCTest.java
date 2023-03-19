f3a9c7da8a82b6096276cdc0b28f2023bd7f1c24
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-03-19 10:25:17 +0200

    Initial commit

--- src/test/java/DiscordRPCTest.java
+++ src/test/java/DiscordRPCTest.java
@@ -1,5 +1,5 @@
-import com.github.kawaxte.drpc.DiscordRPC;
 import com.github.kawaxte.drpc.DiscordEventHandlers;
+import com.github.kawaxte.drpc.DiscordRPC;
 import com.github.kawaxte.drpc.DiscordRichPresence;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -8,38 +8,25 @@ import java.util.concurrent.TimeUnit;
 public class DiscordRPCTest {
 
   public static void main(String... args) {
-    Runtime.getRuntime().addShutdownHook(Executors.defaultThreadFactory()
-        .newThread(DiscordRPC::shutdown));
+    Runtime.getRuntime().addShutdownHook(Executors.defaultThreadFactory().newThread(
+        DiscordRPC::shutdown));
 
     initDiscord();
 
     ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
-    service.scheduleAtFixedRate(DiscordRPC::runCallbacks,
-        2,
-        2,
-        TimeUnit.SECONDS);
+    service.scheduleAtFixedRate(DiscordRPC::runCallbacks, 2, 2, TimeUnit.SECONDS);
   }
 
   static void initDiscord() {
     DiscordEventHandlers handlers = new DiscordEventHandlers();
     handlers.ready = request -> {
-      System.out.printf("%s#%s (%s)%n",
-          request.username,
-          request.discriminator,
-          request.userId);
-
       DiscordRichPresence presence = new DiscordRichPresence.Builder()
-          .setState("Sample state")
-          .setDetails("Sample details")
+          .setState("DiscordRPC Test")
+          .setDetails("Running the RPC test")
           .setStartTimestamp(System.currentTimeMillis() / 1000L)
-          .setPartySize(1)
-          .setPartyMax(4)
           .build();
-
       DiscordRPC.updatePresence(presence);
     };
-
-    //DiscordRPC.initialize("0000000000000000000", handlers, true, null);
-    DiscordRPC.initialize("1058488125313253457", handlers, true, null);
+    DiscordRPC.initialise("0000000000000000000", handlers, true, null);
   }
 }
