c51ef59b385ce087052eca4be2fe033c686a3372
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-04-09 11:52:09 +0300

    Replace `<code></code>` with `{@code }`

--- src/main/java/io/github/kawaxte/presence/DiscordEventHandlers.java
+++ src/main/java/io/github/kawaxte/presence/DiscordEventHandlers.java
@@ -5,7 +5,7 @@ import com.sun.jna.Structure;
 import com.sun.jna.Structure.FieldOrder;
 
 /**
- * The <code>DiscordEventHandlers</code> class is used to register callbacks for events that occur
+ * The {@code DiscordEventHandlers} class is used to register callbacks for events that occur
  * in the Discord Rich Presence API.
  *
  * @author Kawaxte
@@ -22,7 +22,7 @@ public class DiscordEventHandlers extends Structure {
   public IJoinRequestCallback joinRequest;
 
   /**
-   * The <code>IReadyCallback</code> interface is used to register a callback for when the
+   * The {@code IReadyCallback} interface is used to register a callback for when the
    * connection to Discord has been established.
    *
    * @see Callback
@@ -32,14 +32,14 @@ public class DiscordEventHandlers extends Structure {
     /**
      * This method is called when the connection to Discord has been established.
      *
-     * @param request The <code>DiscordUser</code> object containing all required information about
+     * @param request The {@code DiscordUser} object containing all required information about
      *                the user executing the app.
      */
     void onReady(final DiscordUser request);
   }
 
   /**
-   * The <code>IDisconnectedCallback</code> interface is used to register a callback for when the
+   * The {@code IDisconnectedCallback} interface is used to register a callback for when the
    * connection to Discord has been lost.
    *
    * @see Callback
@@ -56,7 +56,7 @@ public class DiscordEventHandlers extends Structure {
   }
 
   /**
-   * The <code>IErroredCallback</code> interface is used to register a callback for when an error
+   * The {@code IErroredCallback} interface is used to register a callback for when an error
    * occurs.
    *
    * @see Callback
@@ -66,15 +66,15 @@ public class DiscordEventHandlers extends Structure {
     /**
      * This method is called when an error occurs.
      *
-     * @param errorCode The <code>int</code> object representing the error code returned.
-     * @param message   The <code>String</code> object representing the message containing details
+     * @param errorCode The {@code int} object representing the error code returned.
+     * @param message   The {@code String} object representing the message containing details
      *                  about the error.
      */
     void onErrored(int errorCode, final String message);
   }
 
   /**
-   * The <code>IJoinGameCallback</code> interface is used to register a callback for when another
+   * The {@code IJoinGameCallback} interface is used to register a callback for when another
    * player joins a game.
    *
    * @see Callback
@@ -84,14 +84,14 @@ public class DiscordEventHandlers extends Structure {
     /**
      * This method is called when another player joins a game.
      *
-     * @param joinSecret The <code>String</code> object representing the secret used to join a
+     * @param joinSecret The {@code String} object representing the secret used to join a
      *                   game.
      */
     void onJoinGame(final String joinSecret);
   }
 
   /**
-   * The <code>ISpectateGameCallback</code> interface is used to register a callback for the when
+   * The {@code ISpectateGameCallback} interface is used to register a callback for the when
    * another player requests to spectate a game.
    *
    * @see Callback
@@ -101,14 +101,14 @@ public class DiscordEventHandlers extends Structure {
     /**
      * This method is called when another player requests to spectate a game.
      *
-     * @param spectateSecret The <code>String</code> object representing the secret used to spectate
+     * @param spectateSecret The {@code String} object representing the secret used to spectate
      *                       a game.
      */
     void onSpectateGame(final String spectateSecret);
   }
 
   /**
-   * The <code>IJoinRequestCallback</code> interface is used to register a callback for the when
+   * The {@code IJoinRequestCallback} interface is used to register a callback for the when
    * another player requests to join a game.
    *
    * @see Callback
@@ -118,7 +118,7 @@ public class DiscordEventHandlers extends Structure {
     /**
      * This method is called when another player requests to join a game.
      *
-     * @param request The <code>DiscordUser</code> object containing all required information about
+     * @param request The {@code DiscordUser} object containing all required information about
      *                the user requesting to join.
      */
     void onJoinRequest(final DiscordUser request);
